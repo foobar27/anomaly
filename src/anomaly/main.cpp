f26f4fb9427b392cc1f3b1de8e99d6ac5ed5bce7
Sébastien Wagener <sebastien.wagener@gmail.com>
2020-05-09 18:18:37 +0200

    Experiment with median absolute deviation estimation

--- src/anomaly/main.cpp
+++ src/anomaly/main.cpp
@@ -2,9 +2,37 @@
 
 #include <fstream>
 #include <vector>
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
 
 using namespace std;
 using namespace anomaly::whisper::model;
+using anomaly::core::timeseries::TimeSeries;
+
+class MedianEstimator {
+public:
+    void operator()(double value) {
+        if (m_first) {
+            m_estimate = value;
+            m_first    = false;
+        } else {
+            if (value > m_estimate) {
+                m_estimate++;
+            } else if (value < m_estimate) {
+                m_estimate--;
+            }
+        }
+    }
+
+    double operator*() const {
+        return m_estimate;
+    }
+
+private:
+    bool   m_first = true;
+    double m_estimate;
+};
 
 int main() {
     ifstream is("/home/sebastien/percent.wsp", ifstream::binary);
@@ -32,20 +60,31 @@ int main() {
 
         Archive archive{archive_info};
         archive.readPoints(is);
-        for (const auto& point : archive.m_points) {
-            os << point.m_timestamp << endl;
-        }
         auto time_series = archive.createTimeSeries();
-        // TODO(sw) implement a range?
+
+        using Accumulator = boost::accumulators::accumulator_set<double, boost::accumulators::features<boost::accumulators::tag::mean>>;
+        int                 number_of_bins = 1440;
+        vector<Accumulator> bins(number_of_bins);
         for (auto point : time_series.allPoints()) {
-            auto val = point.m_value;
-            cout << "time_series[" << point.m_timestamp << "]=";
-            if (val) {
-                cout << *val;
-            } else {
-                cout << "null";
+            auto bin = (point.m_timestamp / time_series.getConfig().m_stepInSeconds) % number_of_bins;
+            if (point.m_value) {
+                bins[bin](*point.m_value);
             }
-            cout << std::endl;
+        }
+
+        // TODO(sw) implement a range?
+        MedianEstimator median;
+        MedianEstimator median_deviation;
+        for (auto point : time_series.allPoints()) {
+            if (point.m_value) {
+                double val    = *point.m_value;
+                auto   bin    = (point.m_timestamp / time_series.getConfig().m_stepInSeconds) % number_of_bins;
+                auto   season = boost::accumulators::mean(bins[bin]);
+                median(val);
+                double deviation = abs(val - *median);
+                media_deviation(deviation);
+                os << point.m_timestamp << "\t" << *point.m_value << "\t" << *median << "\t" << *median_deviation << "\t" << season << endl;
+            }
         }
     }
 
